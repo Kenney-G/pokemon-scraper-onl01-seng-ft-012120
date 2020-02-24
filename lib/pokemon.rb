71a16c25edfbccbf29f96b559fe24dc1cb5735c0
Ken Goad <goadke@g.cofc.edu>
2020-02-24 04:42:17 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,6 +19,7 @@ class Pokemon
   def self.find(pokemon_id, db)
     poke_card = db.execute("
 	  SELECT * FROM pokemon WHERE id=?", pokemon_id).flatten
+	  binding.pry
 	  Pokemon.new(id: poke_card[0], name: poke_card[1], type: poke_card[3], db: db)
   end  
     
